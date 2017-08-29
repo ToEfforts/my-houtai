b1880d199e938dad94403cf879abe4a81e191548
wangtao <1099177965@qq.com>
2017-08-29 23:17:12 +0800

    模块化登录退出功能成功

--- public/js/common.js
+++ public/js/common.js
@@ -1,24 +1,43 @@
+define(['jquery','cookie'], function ($) {
+	//NProgress.start();
+	//NProgress.done();
+	// 控制左侧导航菜单折叠展开
+	$('.navs ul').prev('a').on('click', function () {
+		$(this).next().slideToggle();
+	});
 
-NProgress.start();
-NProgress.done();
-
-$('.navs ul').prev('a').on('click', function () {
-	$(this).next().slideToggle();
-});
-
-// 实现退出功能
-$('#logoutBtn').click(function () {
-	$.ajax({
-		type:'post',
-		url:'/api/logout',
-		dataType:'json',
-		success : function (data) {
-			if (data.code == 200){
-				// success OK
-				location.href = '/main/login';
+	// 实现退出功能
+	$('#logoutBtn').click(function () {
+		$.ajax({
+			type:'post',
+			url:'/api/logout',
+			dataType:'json',
+			success : function (data) {
+				if (data.code == 200){
+					// success OK
+					location.href = '/main/login';
+				}
 			}
-		}
-	})
-            console.log(123);
-	return false;
+		})
+		console.log(123);
+		return false;
+	});
+
+	// 验证是否登录
+	var seesionId = $.cookie('PHPSESSID');
+	if (!seesionId && location.pathname != '/main/login'){
+		location.href = '/main/login';
+	}
+
+	// 获取用户登录信息,并填充页面
+	var loginInfo = JSON.parse($.cookie('loginInfo'));
+	$('.profile img').attr('src',loginInfo.tc_avatar);
+	$('.profile h4').html(loginInfo.tc_name);
+
 });
+
+
+
+
+
+
