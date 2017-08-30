a0bf148d89d399b79abd7222a70b53301d31bdb7
wangtao <1099177965@qq.com>
2017-08-30 16:47:12 +0800

    使用模板引擎重构登录功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'], function ($) {
+define(['jquery','template','cookie'], function ($,template) {
 	//NProgress.start();
 	//NProgress.done();
 	// 控制左侧导航菜单折叠展开
@@ -30,10 +30,13 @@ define(['jquery','cookie'], function ($) {
 	}
 
 	// 获取用户登录信息,并填充页面
-	var loginInfo = JSON.parse($.cookie('loginInfo'));
-	$('.profile img').attr('src',loginInfo.tc_avatar);
-	$('.profile h4').html(loginInfo.tc_name);
-
+	var cookie = $.cookie('loginInfo');
+	var loginInfo = cookie ? JSON.parse(cookie) : {};
+	//$('.profile img').attr('src',loginInfo.tc_avatar);
+	//$('.profile h4').html(loginInfo.tc_name);
+	var tpl = '<div class="avatar img-circle"><img src="{{tc_avatar}}"></div> <h4>{{tc_name}}</h4>'
+	var html = template.render(tpl,loginInfo);
+	$('#profileId').html(html);
 });
 
 
