8f1e8d159a8fb31c5822a7e6ca37b67388d18a9e
wangtao <1099177965@qq.com>
2017-08-30 20:44:06 +0800

    完成启用注销讲师功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -21,8 +21,29 @@ define(['jquery','template','bootstrap'], function ($,template) {
                         // 解析数据 渲染页面
                         var html = template('modalTpl',data.result);
                         $('#modalInfo').html(html);
-                        // 显示弹窗
+                        // 显示弹窗 识别为模态框
                         $('#teacherModal').modal();
+                    }
+                })
+            });
+            // 处理启用注销功能
+            $('.eod').click(function () {
+                var td = $(this).closest('td');
+                var tcId = td.attr('data-tcId');
+                var tcStatus = td.attr('data-status');
+                var that = this; // 点击按钮
+                $.ajax({
+                    type : 'post',
+                    url : '/api/teacher/handle',
+                    data : { tc_id : tcId,tc_status : tcStatus },
+                    dataType : 'json',
+                    success : function (data) {
+                        td.attr('data-status',data.result.tc_status);
+                        if (data.result.tc_status == 0){
+                            $(that).html('注 销');
+                        }else{
+                            $(that).html('启 用');
+                        }
                     }
                 })
             })
